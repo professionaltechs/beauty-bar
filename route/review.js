b5a7fd6dbd05ff978c2db41b642f8929e1f9d9c2
professionaltechs <professionalstechs@gmail.com>
2023-03-08 01:44:41 +0500

    changes

--- route/review.js
+++ route/review.js
@@ -6,10 +6,48 @@ const router = express.Router();
 
 /**
  * @swagger
+ * /api/review/createReview:
+ *  post:
+ *    tags:
+ *      - admin
+ *    security: 
+ *      - bearerAuth: []
+ *    description: Create new review
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            required:
+ *              - userId
+ *              - productId
+ *              - rating
+ *            properties:
+ *              userId:
+ *                type: string
+ *              productId:
+ *                type: string
+ *              rating:
+ *                type: integer
+ *              comment:
+ *                type: string
+ *    responses:
+ *      '200':
+ *        description: 200 OK response
+ *      '404':
+ *        description: Not Found
+ *      '500':
+ *        description: Internal Server Error
+ */
+router.post("/createReview", auth,reviewController.createReview);
+
+/**
+ * @swagger
  * /api/review/getReviewById:
  *  post:
  *    tags:
- *      - user
+ *      - admin
  *    security:
  *      - bearerAuth: []
  *    description: get review by id
@@ -39,7 +77,8 @@ router.post("/getReviewById", auth,reviewController.getReviewById);
  * /api/review/getAllReviews:
  *  post:
  *    tags:
- *      - user
+ *      
+ *      - admin
  *    security:
  *      - bearerAuth: []
  *    description: get all reviews
@@ -52,5 +91,73 @@ router.post("/getReviewById", auth,reviewController.getReviewById);
  *        description: Internal Server Error
  */
 router.post("/getAllReviews", auth, reviewController.getAllReviews);
+
+/**
+ * @swagger
+ * /api/review/updateReviewById:
+ *  post:
+ *    tags:
+ *      - admin
+ *    security: 
+ *      - bearerAuth: []
+ *    description: update review by id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            required:
+ *              - id
+ *            properties:
+ *              id:
+ *                type: string
+ *              userId:
+ *                type: string
+ *              productId:
+ *                type: string
+ *              rating:
+ *                type: integer
+ *              comment:
+ *                type: string
+ *    responses:
+ *      '200':
+ *        description: 200 OK response
+ *      '404':
+ *        description: Not Found
+ *      '500':
+ *        description: Internal Server Error
+ */
+router.post("/updateReviewById", auth, reviewController.updateReviewById);
+
+/**
+ * @swagger
+ * /api/review/deleteReviewById:
+ *  post:
+ *    tags:
+ *      - admin
+ *    security:
+ *      - bearerAuth: []
+ *    description: delete review by id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            required:
+ *              - id
+ *            properties:
+ *              id:
+ *                type: string
+ *    responses:
+ *      '200':
+ *        description: 200 OK response
+ *      '404':
+ *        description: Not Found
+ *      '500':
+ *        description: Internal Server Error
+ */
+router.post("/deleteReviewById", auth, reviewController.deleteReviewById);
 
 module.exports = router;
